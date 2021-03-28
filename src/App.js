0b7a12871998931eb0d9bba838979317183e044c
karahman <jakarahman007@gmail.com>
2021-03-28 13:09:54 +0700

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
-  );
+    <Router>
+      <div className="App">
+        {/* Router */}
+        <Switch>
+          
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
