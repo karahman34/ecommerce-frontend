98bae23fab7803bc45e05487a67207d0add64e4f
karahman <jakarahman007@gmail.com>
2021-04-08 17:44:49 +0700

    add placeholder prop

--- src/components/layouts/default/SearchBox/SearchBox.js
+++ src/components/layouts/default/SearchBox/SearchBox.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import SearchBoxStyles from "./SearchBox.module.scss";
 
-const SearchBox = ({ onSearch }) => {
+const SearchBox = ({ onSearch, placeholder }) => {
   const [search, setSearch] = useState("");
 
   function submitHandler(e) {
@@ -19,7 +19,7 @@ const SearchBox = ({ onSearch }) => {
         type='text'
         className={SearchBoxStyles.input}
         value={search}
-        placeholder='Enter a keyword..'
+        placeholder={placeholder || "Enter a keyword.."}
         onChange={(e) => setSearch(e.target.value)}
       />
       <button type='submit' className={SearchBoxStyles.magnify}>
@@ -31,6 +31,7 @@ const SearchBox = ({ onSearch }) => {
 
 SearchBox.propTypes = {
   onSearch: PropTypes.func,
+  placeholder: PropTypes.string,
 };
 
 export default SearchBox;
